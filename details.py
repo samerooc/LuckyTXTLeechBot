3491c2b10018ec29d0b60d0d80a0a4804f95901c
samerooc <roocrolex@gmail.com>
2025-01-26 12:50:46 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 27881923
-api_hash = "79abda5e46a51fc0dce1313f2548ce19"
-bot_token = "6263666911:AAF_Zm2baKNCJMGPn8aCBlVArvTa-A4kz1k"
+api_id = 26343810
+api_hash = "6ac64c03481d679241dd749b8ecee201"
+bot_token = "7924569463:AAH6e-y5CE8Z_iTXM_-fL_AREBg5pHQ2ihg"
 
